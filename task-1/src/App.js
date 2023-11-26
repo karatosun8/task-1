329c8a0df77c476b665cf9f81f494896d9fdf903
karatosun8 <sefa_karatosun@hotmail.com>
2023-11-26 14:46:03 +0300

    start

---
+++ task-1/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Chart from './components/Chart';
+import Butons from './components/Butons';
+
+function App() {
+  return (
+    <div className="App">
+      <Chart/>
+      <Butons/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ task-1/src/components/Butons.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Butons = () => {
+  return (
+    <div>buttons</div>
+  )
+}
+
+export default Butons
